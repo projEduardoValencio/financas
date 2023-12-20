d16cdf4f8029e604e6972848d59754c532ae3d0b
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2023-12-19 22:23:43 -0300

    feat: cards layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,25 +10,29 @@ const IndexDashboard : React.FC = () =>{
             <SideBar />
           </SideBarProvider>
 
-          <div className="h-screen w-full">
-            <DashBoardCard
-              color="blue"
-              label="Saldo atual"
-              value={1230.982}
-              icon={<WalletRounded style={{color: "white"}}/>}
-            />
-            <DashBoardCard
-              color="green"
-              label="Saldo atual"
-              value={1230.982}
-              icon={<ArrowDownward style={{color: "white"}}/>}
-            />
-            <DashBoardCard
-              color="red"
-              label="Saldo atual"
-              value={1230.982}
-              icon={<ArrowUpward style={{color: "white"}}/>}
-            />
+          <div className="h-screen w-full justify-center p-10">
+            <div className="flex flex-row gap-5 justify-center">
+              <DashBoardCard
+                color="blue"
+                label="Saldo atual"
+                value={1230.982}
+                icon={<WalletRounded style={{color: "white"}}/>}
+              />
+              
+              <DashBoardCard
+                color="green"
+                label="Receitas"
+                value={1230.982}
+                icon={<ArrowDownward style={{color: "white"}}/>}
+              />
+
+              <DashBoardCard
+                color="red"
+                label="Despesas"
+                value={1230.982}
+                icon={<ArrowUpward style={{color: "white"}}/>}
+              />
+            </div>
           </div>
         </main>
     )
